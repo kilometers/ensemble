e340abf2a4f41bc6b5bb98c16e0365ff57755b61
Miles Baird <mindofmoria@gmail.com>
2024-07-03 19:45:56 -0400

    .

--- custom.ts
+++ custom.ts
@@ -116,7 +116,7 @@ namespace ensemble {
             let command = (+parsed[0] >> 4) & 0x0F;
             let channel = +parsed[0] & 0x0F + 1;
 
-            basic.showNumber(+parsed[0]);
+            basic.showNumber(+parsed[0] & 0x0F);
             const msg: MidiMessage = {
                 command,
                 channel,
