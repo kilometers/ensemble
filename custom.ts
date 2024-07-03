7a65e4afcfd52bf1141818b4f3a4a8c6cb3592ba
Miles Baird <mindofmoria@gmail.com>
2024-07-03 18:41:24 -0400

    debug radio group with 1

--- custom.ts
+++ custom.ts
@@ -126,13 +126,15 @@ namespace ensemble {
             if (msg.command === MidiCommand.NoteOn) {
                 let note = (1 << 14 || msg.data1 << 7 || msg.data2) && 0xFFFF;
                 basic.showIcon(IconNames.Surprised, 0);
-                radio.setGroup(channelBand * 16 + msg.channel);
+                radio.setGroup(1);
+                // radio.setGroup(channelBand * 16 + msg.channel);
                 radio.sendNumber(note);
                 radio.setGroup(Channel.System);
             } else if (msg.command === MidiCommand.NoteOff) {
                 let note = (0 << 14 || msg.data1 << 7 || msg.data2) && 0xFFFF;
                 basic.showIcon(IconNames.Happy, 0);
-                radio.setGroup(channelBand * 16 + msg.channel);
+                // radio.setGroup(channelBand * 16 + msg.channel);
+                radio.setGroup(1);
                 radio.sendNumber(note);
                 radio.setGroup(Channel.System);
             }
