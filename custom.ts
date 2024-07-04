63fffd9b29959e1a258bb4e5f25deb38f15b7d9e
Miles Baird <mindofmoria@gmail.com>
2024-07-03 21:33:15 -0400

    use proper bitwise notation

--- custom.ts
+++ custom.ts
@@ -124,14 +124,14 @@ namespace ensemble {
             }
 
             if (msg.command === MidiCommand.NoteOn) {
-                let note = (1 << 14 || msg.data1 << 7 || msg.data2) & 0xFFFF;
+                let note = (1 << 14 | msg.data1 << 7 | msg.data2) & 0xFFFF;
                 basic.showIcon(IconNames.Surprised, 0);
                 basic.showNumber(note);
                 radio.setGroup(channelBand * 16 + msg.channel);
                 radio.sendNumber(note);
                 radio.setGroup(Channel.System);
             } else if (msg.command === MidiCommand.NoteOff) {
-                let note = (0 << 14 || msg.data1 << 7 || msg.data2) & 0xFFFF;
+                let note = (0 << 14 | msg.data1 << 7 | msg.data2) & 0xFFFF;
                 basic.showIcon(IconNames.Happy, 0);
                 radio.setGroup(channelBand * 16 + msg.channel);
                 radio.sendNumber(note);
