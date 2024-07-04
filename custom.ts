56e159d4a45e3be9ac3cc1b859cec723cf57bbb6
Miles Baird <mindofmoria@gmail.com>
2024-07-03 21:35:39 -0400

    Replace OR AND operators with correct bitwise operators

--- custom.ts
+++ custom.ts
@@ -126,7 +126,6 @@ namespace ensemble {
             if (msg.command === MidiCommand.NoteOn) {
                 let note = (1 << 14 | msg.data1 << 7 | msg.data2) & 0xFFFF;
                 basic.showIcon(IconNames.Surprised, 0);
-                basic.showNumber(note);
                 radio.setGroup(channelBand * 16 + msg.channel);
                 radio.sendNumber(note);
                 radio.setGroup(Channel.System);
